59a1eff41200df8f9dcc1fc37e20967ec79d92dd
GANESH <@desire-674>
2026-03-02 14:21:34 +0530

    fibanocci series code added

---
+++ src/fibanocci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class fibanocci {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+System.out.print("Enter a number: ");
+int n = sc.nextInt();
+int a=0,b=1;
+System.out.print("Fibanocci series: ");
+for(int i=0;i<n;i++)
+{
+    System.out.print(a + " ");
+    int c=a+b;
+    a=b;
+    b=c;
+}
+
+    }
+}
